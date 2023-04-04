d1ed579a5ed506c09e3dc51f5c2fc362cc84aaa1
Pratik <Pratik@LAPTOP-9GJ0492B>
2023-04-04 12:43:09 +0530

    Day one first commit

---
+++ eclipse-workspace/M-29 core java/dayone/src/org/tnsif/introduction/FirstProgram.java
@@ -0,0 +1,11 @@
+package org.tnsif.introduction;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
